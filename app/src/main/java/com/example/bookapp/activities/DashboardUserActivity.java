a84f8c73eaf29112e1da2d67caee41cb82d21a6e
duongphan <duong.pnt@savvycom.vn>
2025-06-24 19:35:37 +0700

    create show book user details

--- app/src/main/java/com/example/bookapp/activities/DashboardUserActivity.java
+++ app/src/main/java/com/example/bookapp/activities/DashboardUserActivity.java
@@ -1,16 +1,35 @@
 package com.example.bookapp.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.bookapp.BooksUserFragment;
 import com.example.bookapp.databinding.ActivityDashboardUserBinding;
+import com.example.bookapp.models.ModelCategory;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DashboardUserActivity extends AppCompatActivity {
+
+    //to show in tabs
+    public ArrayList<ModelCategory> categoryArrayList;
+    public  ViewPagerAdapter viewPagerAdapter;
+
 
     //view binding
     private ActivityDashboardUserBinding binding;
@@ -28,6 +47,9 @@ public class DashboardUserActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
 
+        setupViewPagerAdapter(binding.viewPager);
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
+
         //handle click, logout
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +59,116 @@ public class DashboardUserActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void setupViewPagerAdapter(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,this
+
+        );
+
+        categoryArrayList = new ArrayList<>();
+
+        // Load categories from Firebase
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                // Clear before adding to list
+                categoryArrayList.clear();
+
+                // Load Categories - Static e.g. All, Most Viewed
+                // Add data to models
+                ModelCategory modelAll = new ModelCategory("01", "ALL", "", 1);
+                ModelCategory modelMostViewed = new ModelCategory("02", "Most Viewed", "", 1);
+                ModelCategory modelMostDownloaded = new ModelCategory("03", "Most Downloaded", "", 1);
+
+                //add models to list
+                categoryArrayList.add(modelAll);
+                categoryArrayList.add(modelMostViewed);
+                categoryArrayList.add(modelMostDownloaded);
+
+                viewPagerAdapter.addFragment(BooksUserFragment.newInstance(
+                        ""+modelAll.getId(),
+                        ""+modelAll.getCategory(),
+                        ""+modelAll.getUid()
+
+                ), modelAll.getCategory());
+
+                viewPagerAdapter.addFragment(BooksUserFragment.newInstance(
+                        ""+modelMostViewed.getId(),
+                        ""+modelAll.getCategory(),
+                        ""+modelMostViewed.getUid()
+
+                ), modelMostViewed.getCategory());
+
+                viewPagerAdapter.addFragment(BooksUserFragment.newInstance(
+                        ""+modelMostViewed.getId(),
+                        ""+modelAll.getCategory(),
+                        ""+modelMostViewed.getUid()
+
+                ), modelMostDownloaded.getCategory());
+
+
+                viewPagerAdapter.notifyDataSetChanged();
+
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+                    categoryArrayList.add(model);
+                    viewPagerAdapter.addFragment(BooksUserFragment.newInstance(
+                            ""+model.getId(),
+                            ""+model.getCategory(),
+                            ""+model.getUid()),
+                            model.getCategory());
+
+                    viewPagerAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private ArrayList<BooksUserFragment> fragmentList = new ArrayList<>();
+        private ArrayList<String> fragmentTitleList = new ArrayList<>();
+        private Context context;
+
+        public ViewPagerAdapter(FragmentManager fm, int behavior, DashboardUserActivity dashboardUserActivity) {
+            super(fm, behavior);
+            this.context = context;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        private void addFragment(BooksUserFragment fragment, String title){
+            //add fragment
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return fragmentTitleList.get(position);
+        }
+    }
+
+
 
     private void checkUser() {
         // get current user
---
+++ app/src/main/java/com/example/bookapp/filters/FilterPdfUser.java
@@ -0,0 +1,49 @@
+package com.example.bookapp.filters;
+
+import android.widget.Filter;
+
+import com.example.bookapp.adapters.AdapterPdfUser;
+import com.example.bookapp.models.ModelPdf;
+
+import java.util.ArrayList;
+
+public class FilterPdfUser extends Filter {
+
+    ArrayList<ModelPdf> filterList;
+    AdapterPdfUser adapterPdfUser;
+
+    public FilterPdfUser(ArrayList<ModelPdf> filterList, AdapterPdfUser adapterPdfUser) {
+        this.filterList = filterList;
+        this.adapterPdfUser = adapterPdfUser;
+    }
+
+    @Override
+    protected FilterResults performFiltering(CharSequence constraint) {
+        FilterResults results = new FilterResults();
+
+        if (constraint != null && constraint.length() > 0) {
+            constraint = constraint.toString().toUpperCase();
+            ArrayList<ModelPdf> filteredModels = new ArrayList<>();
+
+            for (ModelPdf model : filterList) {
+                if (model.getTitle().toUpperCase().contains(constraint)) {
+                    filteredModels.add(model);
+                }
+            }
+
+            results.count = filteredModels.size();
+            results.values = filteredModels;
+        } else {
+            results.count = filterList.size();
+            results.values = filterList;
+        }
+
+        return results;
+    }
+
+    @Override
+    protected void publishResults(CharSequence constraint, FilterResults results) {
+        adapterPdfUser.pdfArrayList = (ArrayList<ModelPdf>) results.values;
+        adapterPdfUser.notifyDataSetChanged();
+    }
+}
